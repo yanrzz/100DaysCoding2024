bcda79542b6ed513d503865cecaeccb636b2f922
Yusran <jhyyusran766@gmail.com>
2024-09-02 08:34:47 +0800

    Update Class.java

--- pkgclass/Class.java
+++ pkgclass/Class.java
@@ -1,20 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package pkgclass;
-
-/**
- *
- * @author LENOVO
- */
 public class Class {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+public static void main(String[] args) 
     }
     
 }

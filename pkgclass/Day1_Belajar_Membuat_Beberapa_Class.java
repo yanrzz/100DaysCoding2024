bf18652d555c412126426891ea3849fd4716170a
Yusran <jhyyusran766@gmail.com>
2024-09-02 11:08:19 +0800

    Update Day1_Belajar_Membuat_Beberapa_Class.java

--- pkgclass/Day1_Belajar_Membuat_Beberapa_Class.java
+++ pkgclass/Day1_Belajar_Membuat_Beberapa_Class.java
@@ -1,7 +1,23 @@
 
 package pkgclass;
 public class Class {
-public static void main(String[] args) 
+public static void main(String[] args) {
+     String nama = "Yusran";
+    String agama = "Islam";
+    String jenis_kelamin = "Laki-Laki";
+    String fakultas = "Teknik";
+    String kelas = "informatika B";
+    int umur = 17;
+    String ttl = "kampuno/04-10-2006";
+    
+    System.out.println("nama: "+nama);
+    System.out.println("agama: "+agama);
+    System.out.println("jenis kelamin: "+jenis_kelamin);
+    System.out.println("fakultas: "+fakultas);
+    System.out.println("kelas: "+kelas);
+    System.out.println("umur: "+umur+ " tahun");
+    System.out.println("tempat/tanggal lahir: "+ ttl);
+
     }
     
 }

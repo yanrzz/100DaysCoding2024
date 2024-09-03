97bcdc9a3af24cc3465d13553271446619c7df93
Yusran <jhyyusran766@gmail.com>
2024-09-04 07:18:07 +0800

    Day3

---
+++ Main.java
@@ -0,0 +1,15 @@
+
+package pkg100dayscoding;
+
+public class Main {
+
+        public static void main(String[] args) {
+            System.out.println("Nama = Yusran");
+            System.out.println("Alamat = Wonomulyo");
+            System.out.println("TTL = Kampuno 4 Oktober 2006");
+            System.out.println("Jenis Kelamin = Laki-laki");
+            System.out.println("Agama = Islam");
+            System.out.println("Umur = 17 tahun");
+    }
+    
+}

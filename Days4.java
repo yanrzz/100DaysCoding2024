40c55e40553bfcb9549817e8ce836f00ee650d0a
Yusran <jhyyusran766@gmail.com>
2024-09-05 10:51:02 +0800

    Day4

---
+++ Days4.java
@@ -0,0 +1,9 @@
+
+package pkg100dayscoding;
+public class Days4 {
+    public static void main(String[] args) {
+        System.out.print("Assalamualaikum, perkenalkan:");
+        System.out.printf(" nama saya %s dan saya berumur %d tahun.", "Yusran", 17);
+    }
+    
+}

a21879c4feeeef78db0d58f90541fb79af546590
Yusran <jhyyusran766@gmail.com>
2024-09-06 14:17:22 +0800

    Day5

---
+++ Days5.java
@@ -0,0 +1,20 @@
+
+package pkg100dayscoding;
+public class Days5 {
+    public static void main(String[] args) {
+    System.out.print("Nama Yusran");
+    // digunakan untuk mencetak text ke konsol tidak di ikuti dengan baris baru
+    System.out.println("\nkelas: B angkatan: 2024 ");
+    // digunkan unutk mencetak text ke konsol di ikuti dengan baris baru
+    System.out.printf("saya berasal dari %s dan saya berumur %d tahun.", "Wonomulyo", 17);
+    /* digunakan untuk mencetak text dengan format
+        %s = String untuk huruf
+        %d = untuk bilang bulat
+        %f = untuk float bilangan berkoma
+        %n = enter untuk baris baru
+        %b = boolean untuk true and false
+    */
+    
+    }
+    
+}

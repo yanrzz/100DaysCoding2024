0da3b3334cc5f816b3767446ef414eddf5a107a1
Yusran <jhyyusran766@gmail.com>
2024-09-08 10:41:21 +0800

    Day 7

---
+++ Days7.java
@@ -0,0 +1,21 @@
+package pkg100dayscoding;
+public class Days7 {
+    public static void main(String[] args) {
+        String nama = "Yusran";
+        int umur = 17;
+        double tinggiBadan = 170.1;
+        float beratBadan = 43.2f;
+        boolean javaSusah = true;
+        char golonganDarah = 'B';
+        System.out.println("nama: "+ nama);
+        System.out.println("umur: "+ umur);
+        System.out.println("Tinggi Badan: "+ tinggiBadan);
+        System.out.println("Berat Badan "+ beratBadan);
+        System.out.println("java itu susah: "+ javaSusah);
+        System.out.println("golongan darah: "+ golonganDarah);
+        
+        
+        
+    }
+    
+}

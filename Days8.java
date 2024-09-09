c3d94e545823c70ee0a8eb590205655e6be6635d
Yusran <jhyyusran766@gmail.com>
2024-09-09 13:20:21 +0800

    Day8

---
+++ Days8.java
@@ -0,0 +1,19 @@
+
+package pkg100dayscoding;
+public class Days8 {
+    public static void main(String[] args) {
+        String nama = "Yusran";
+        System.out.println("Nama: "+ nama);
+        // sebelum update
+        nama = "Asep";
+        System.out.println("Nama setelah update: "+ nama);
+        //setelah update
+        int umur = 17;
+        System.out.println("umur: "+ umur);
+        // sebelum update
+        umur = 70;
+        System.out.println("umur setelah update: "+ umur);
+        
+    }
+    
+}

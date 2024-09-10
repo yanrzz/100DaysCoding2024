0039c9582b6697d58b2657d8f2cb8ed366a6a785
Yusran <jhyyusran766@gmail.com>
2024-09-10 11:46:05 +0800

    Day9

---
+++ Days9.java
@@ -0,0 +1,11 @@
+
+package pkg100dayscoding;
+
+public class Days9 {
+    public static void main(String[] args) {
+        final double phi = 3.14;
+        System.out.println("nilai final phi adalah "+ phi);
+            }
+    
+    
+}

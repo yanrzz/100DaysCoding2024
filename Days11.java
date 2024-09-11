89d1c8ecc1b284449b5e983b0e1d0c5e7922af38
Yusran <jhyyusran766@gmail.com>
2024-09-12 00:06:10 +0800

    Day11

---
+++ Days11.java
@@ -0,0 +1,11 @@
+
+package pkg100dayscoding;
+public class Days11 {
+    public static void main(String[] args) {
+        short minimal = -32768;
+        short maximal = 32767;
+        System.out.println("Nilai minimal dari short adalah "+ minimal);
+        System.out.println("Nilai maximal dari short adalah "+ maximal);
+    }
+    
+}

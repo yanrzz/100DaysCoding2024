dfa24d391a6036d962cb1af9afa7f3de24536e52
Yusran <jhyyusran766@gmail.com>
2024-09-11 15:57:48 +0800

    Day10

---
+++ Days10.java
@@ -0,0 +1,10 @@
+
+package pkg100dayscoding;
+public class Days10 {
+    public static void main(String[] args) {
+        byte minimal = -128;
+        byte maximal = 127;
+        System.out.println("Nilai minimal dari tipe data byte adalah "+ minimal +" hingga nilai maximal adalah "+ maximal);
+    }
+    
+}

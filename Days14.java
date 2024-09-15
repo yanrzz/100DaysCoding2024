6eb5790f08b942ca88da5ae8eea6c2cd213923b1
Yusran <jhyyusran766@gmail.com>
2024-09-15 09:25:03 +0800

    Day14

---
+++ Days14.java
@@ -0,0 +1,13 @@
+
+package pkg100dayscoding;
+public class Days14 {
+    public static void main(String[] args) {
+        long minimal = -922337203685775808L;
+        long maximal = 922337203685775807L;
+        System.out.println("nilai minimal "+ minimal);
+        System.out.println("nilai maximal "+ maximal);
+        
+        
+    }
+    
+}

807ea5c7fa56ba6b9487ae10ec67b48b17606f5d
Yusran <jhyyusran766@gmail.com>
2024-09-17 18:07:53 +0800

    Day16

---
+++ Days16.java
@@ -0,0 +1,10 @@
+
+package pkg100dayscoding;
+public class Days16 {
+    public static void main(String[] args) {
+        float tinggiBadan = 170.4f;
+        float beratBadan = 43.2f;
+        System.out.println("tinggi badan "+ tinggiBadan +"cm");
+        System.out.println("berat badan "+ beratBadan +"kg");
+    }
+}

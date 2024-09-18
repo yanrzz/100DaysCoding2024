237c1044d7cdb7d6cac4dd8ca777d1fcb86eee40
Yusran <jhyyusran766@gmail.com>
2024-09-18 19:04:27 +0800

    Day17

---
+++ Days17.java
@@ -0,0 +1,12 @@
+
+package pkg100dayscoding;
+
+public class Days17 {
+    public static void main(String[] args) {
+        char inisial = 'Y';
+        char golonganDarah = 'B';
+        System.out.println("inisial nama "+ inisial);
+        System.out.println("golongan darah "+ golonganDarah);
+    }
+    
+}

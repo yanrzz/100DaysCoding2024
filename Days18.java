ea51d49671f33618f7e460097899013ed19ce336
Yusran <jhyyusran766@gmail.com>
2024-09-19 18:55:34 +0800

    Day18

---
+++ Days18.java
@@ -0,0 +1,11 @@
+
+package pkg100dayscoding;
+
+public class Days18 {
+    public static void main(String[] args) {
+        boolean javaItuMudah = false;
+        boolean javaItuSusah = true;
+        System.out.println("java itu mudah "+ javaItuMudah);
+        System.out.println("java itu susah "+ javaItuSusah);
+    }
+}

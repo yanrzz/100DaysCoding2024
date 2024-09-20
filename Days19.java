6e0c993ff8e12b12059360423d3397088ac9269d
Yusran <jhyyusran766@gmail.com>
2024-09-20 13:17:52 +0800

    Day19

---
+++ Days19.java
@@ -0,0 +1,11 @@
+
+package pkg100dayscoding;
+
+public class Days19 {
+    public static void main(String[] args) {
+    String nama = "Yusran";
+        System.out.println("nama "+ nama);
+
+    }
+    
+}

71b8a5ccfa3465e7cf6824b85687239017a97cd1
Yusran <jhyyusran766@gmail.com>
2024-09-21 08:33:00 +0800

    Day20

---
+++ Days20.java
@@ -0,0 +1,10 @@
+
+package pkg100dayscoding;
+
+public class Days20 {
+    public static void main(String[] args) {
+        Byte angka = 20;
+        System.out.println("angka "+ angka);
+        
+    }
+}

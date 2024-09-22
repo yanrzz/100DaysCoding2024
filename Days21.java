9c054a4ad6c771f27a4f55fbe0121f087155decc
Yusran <jhyyusran766@gmail.com>
2024-09-22 16:21:34 +0800

    Day21

---
+++ Days21.java
@@ -0,0 +1,10 @@
+
+package pkg100dayscoding;
+
+public class Days21 {
+    public static void main(String[] args) {
+        Short angka = 130;
+        System.out.println("angka "+ angka);
+    }
+    
+}

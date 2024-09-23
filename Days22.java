d93395e2bf82cc9eee97d731686a6a7d33fc32fe
Yusran <jhyyusran766@gmail.com>
2024-09-23 12:10:10 +0800

    Day22

---
+++ Days22.java
@@ -0,0 +1,10 @@
+
+package pkg100dayscoding;
+public class Days22 {
+    public static void main(String[] args) {
+        Integer var1 = null;
+        System.out.println("nilai kosong "+ var1);
+        
+    }
+    
+}

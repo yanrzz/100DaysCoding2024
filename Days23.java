d07c9a3c0d975349492693ed95307a5f66000f79
Yusran <jhyyusran766@gmail.com>
2024-09-24 00:02:46 +0800

    Day23

---
+++ Days23.java
@@ -0,0 +1,9 @@
+
+package pkg100dayscoding;
+public class Days23 {
+    public static void main(String[] args) {
+        Long angka = 123456789L;
+        System.out.println("angka long "+ angka);
+    }
+    
+}

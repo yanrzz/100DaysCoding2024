4a69170ca818bc4936b5041f55a20d8080f7de53
yusran <jhyyusran766@gmail.com>
2024-09-25 19:30:47 +0800

    Day24

---
+++ Days24.java
@@ -0,0 +1,10 @@
+
+package pkg100dayscoding;
+
+public class Days24 {
+    public static void main(String[] args) {
+        Float angka = 125.9f;
+        System.out.println("angka "+ angka);
+    }
+    
+}

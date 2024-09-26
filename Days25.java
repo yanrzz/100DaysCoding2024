c091215e90961d634f3556a2cd9d249e5b8d3c62
yusran <jhyyusran766@gmail.com>
2024-09-26 18:40:41 +0800

    Add files via upload

---
+++ Days25.java
@@ -0,0 +1,10 @@
+
+package pkg100dayscoding;
+
+public class Days25 {
+    public static void main(String[] args) {
+        Double angka = 3.1450;
+        System.out.println("angka "+ angka);
+    }
+    
+}

d9413fc06a8fbb15cc9db421f67473590b4c23ef
yusran <jhyyusran766@gmail.com>
2024-09-27 18:37:15 +0800

    Day26

---
+++ Days26.java
@@ -0,0 +1,10 @@
+
+package pkg100dayscoding;
+
+public class Days26 {
+    public static void main(String[] args) {
+        Character golonganDarah = 'B';
+        System.out.println("Golongan Darah saya = "+ golonganDarah);
+    }
+    
+}

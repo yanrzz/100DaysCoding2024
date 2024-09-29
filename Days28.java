189cbc05f18e62f2efdc2b0054420788c6e006c0
yusran <jhyyusran766@gmail.com>
2024-09-29 13:22:10 +0800

    Day28

---
+++ Days28.java
@@ -0,0 +1,12 @@
+
+package pkg100dayscoding;
+
+public class Days28 {
+    public static void main(String[] args) {
+        Boolean isFun = false;
+        Boolean isNotFun = true;
+        System.out.println("boolean itu gampang "+ isFun);
+        System.out.println("boolean itu susah "+ isNotFun);
+    }
+    
+}

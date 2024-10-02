10eddcad42b0b78af43ddbae2007b793722a74f3
yusran <jhyyusran766@gmail.com>
2024-10-02 19:35:57 +0800

    Day31

---
+++ Days31.java
@@ -0,0 +1,17 @@
+
+package pkg100dayscoding;
+
+public class Days31 {
+    public static void main(String[] args) {
+        String a = "120";
+        byte b = Byte.parseByte(a);
+        System.out.println(a);
+        short c = Short.parseShort(a);
+        System.out.println(a);
+        int d = Integer.parseInt(a);
+        System.out.println(a);
+        long e = Long.parseLong(a);
+        System.out.println(a);
+    }
+    
+}

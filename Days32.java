35c4120facb8382ca0a4f63f42b9353554f7304b
yusran <jhyyusran766@gmail.com>
2024-10-03 19:24:02 +0800

    Day32

---
+++ Days32.java
@@ -0,0 +1,34 @@
+
+package pkg100dayscoding;
+public class Days32 {
+    public static void main(String[] args) {
+        // bilangan bulat
+        byte a = 40;
+        String aku = Byte.toString(a);
+        System.out.println(aku);
+        short b = 404;
+        String asep = Short.toString(b);
+        System.out.println(asep);
+        int c = 4040;
+        String agus = c + "";
+        System.out.println(agus);
+        long d = 40404;
+        String udin = Long.toString(d);
+        System.out.println(udin);
+        // bilangan decimal
+        float f = 14.0f;
+        String agus1 = String.valueOf(f);
+        System.out.println(agus1);
+        double e = 140.0;
+        String asep1 = String.valueOf(e);
+        System.out.println(asep1);
+        // selain angka
+        char g ='Y';
+        String udin1 = g + "";
+        System.out.println(g);
+        boolean h = false;
+        String asep2 = h + "";
+        System.out.println(asep2);
+    }
+    
+}

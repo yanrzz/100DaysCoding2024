22951afefe33aa3fa673082db7d582b36b65d0de
yusran <jhyyusran766@gmail.com>
2024-10-06 20:06:10 +0800

    Day35

---
+++ Days35.java
@@ -0,0 +1,17 @@
+
+package pkg100dayscoding;
+public class Days35 {
+    public static void main(String[] args) {
+       // tambah
+        int a,b;
+        a = 10;
+        b = 20;
+        System.out.println(a + b);
+        // kurang
+        int d,e,f;
+        d = 20;
+        e = 10;
+        f = d - e;
+        System.out.println(f);
+    }
+}

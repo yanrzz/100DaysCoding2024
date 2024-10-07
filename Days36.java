19e5800884e7216c7279054f83b07a6262312896
yusran <jhyyusran766@gmail.com>
2024-10-07 19:48:38 +0800

    Day36

---
+++ Days36.java
@@ -0,0 +1,18 @@
+
+package pkg100dayscoding;
+public class Days36 {
+    public static void main(String[] args) {
+        // perkalian 
+        int a,b,c,d,e;
+        a = 2;
+        b = 2;
+        c = a * b;
+        d = a / b;
+        e = a % b;
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        
+    }
+    
+}

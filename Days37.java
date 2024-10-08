599b086c9ce9386023ae070f4dd081279d2b5373
yusran <jhyyusran766@gmail.com>
2024-10-08 17:06:17 +0800

    Day 37

---
+++ Days37.java
@@ -0,0 +1,12 @@
+
+package pkg100dayscoding;
+public class Days37 {
+    public static void main(String[] args) {
+        // operator penugasan 
+        int a = 10;
+        a += 2;
+        System.out.println(a);
+        a -= 3;
+        System.out.println(a);
+    }
+}

f107f87552b6b44e637f1642050e46ba825f4cb2
yusran <jhyyusran766@gmail.com>
2024-10-13 20:48:50 +0800

    Day42

---
+++ Days42.java
@@ -0,0 +1,15 @@
+
+package pkg100dayscoding;
+
+public class Days42 {
+    public static void main(String[] args) {
+        int a,b;
+        a = 10;
+        b = 15;
+        System.out.println("apa kah variable a nilai nya sama dengan b : "+ (a == b));
+        System.out.println("apakah nilai a dan b tidak sama dengan : "+ (a != b));
+        
+        
+        
+}
+}

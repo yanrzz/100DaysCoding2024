d5d8f1069e6b956b4d9adbe272fcdc238a9328b7
yusran <jhyyusran766@gmail.com>
2024-10-14 19:41:48 +0800

    Day43

---
+++ Days43.java
@@ -0,0 +1,15 @@
+
+package pkg100dayscoding;
+
+public class Days43 {
+    public static void main(String[] args) {
+        int a,b;
+        a = 10;
+        b = 15;
+        System.out.println("apa kah nilai a lebih besar sama dengan b "+ (a >= b));
+        System.out.println("apa kah nilai a lebih kecil sama dengan b "+ (a <= b));
+        
+        
+    }
+    
+}

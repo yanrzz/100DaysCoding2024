c23f566037583fef7f2545724b322dd97ade6636
yusran <jhyyusran766@gmail.com>
2024-10-15 20:30:46 +0800

    Day44

---
+++ Days44.java
@@ -0,0 +1,13 @@
+
+package pkg100dayscoding;
+
+public class Days44 {
+    public static void main(String[] args) {
+    boolean a,b;
+    a = true;
+    b = false;
+    System.out.println(a && b);
+    
+  } 
+}
+

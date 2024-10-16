dcc3d06d668286c0e230d4d8951e778d14c995bd
yusran <jhyyusran766@gmail.com>
2024-10-16 21:30:20 +0800

    Day45

---
+++ Days45.java
@@ -0,0 +1,10 @@
+
+package pkg100dayscoding;
+
+public class Days45 {
+    public static void main(String[] args) {
+        int a = 9;
+        boolean b = a <= 8 || a <=10;
+        System.out.println(b);
+    }
+}

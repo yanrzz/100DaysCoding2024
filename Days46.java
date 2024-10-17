18bf717daed9857ccbfe3d8133bf0251cf599642
yusran <jhyyusran766@gmail.com>
2024-10-17 20:48:52 +0800

    Day46

---
+++ Days46.java
@@ -0,0 +1,15 @@
+
+package pkg100dayscoding;
+
+import java.util.Scanner;
+
+public class Days46 {
+    public static void main(String[] args) {
+    Scanner b = new Scanner(System.in);
+    System.out.println("masukan boolean a: ");
+    boolean a = b.nextBoolean();
+    System.out.println("maka hasil boolean a adalah" + !a);
+    
+            
+    } 
+}

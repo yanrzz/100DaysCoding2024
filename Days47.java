5a9be378113e1bf6026ad541748686c463b87a6d
yusran <jhyyusran766@gmail.com>
2024-10-18 20:25:02 +0800

    Day47

---
+++ Days47.java
@@ -0,0 +1,15 @@
+
+package pkg100dayscoding;
+
+import java.util.Scanner;
+
+public class Days47 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("masukan angka");
+        int a = input.nextInt();
+        if(a > 10){
+        System.out.println("maka belasan hingga ratusan ");
+        }
+    }
+}

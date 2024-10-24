78af3f5d801e12f07426f17caae226a35660c005
yusran <jhyyusran766@gmail.com>
2024-10-24 20:18:12 +0800

    Day53

---
+++ Days53.java
@@ -0,0 +1,17 @@
+
+package pkg100dayscoding;
+import java.util.*;
+public class Days53 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("nilai awal: ");
+        int a = input.nextInt();
+        System.out.print("nilai akhir: ");
+        int b = input.nextInt();
+        for (int i = a; i <= b; i++) {
+            System.out.println(i+" .yusran");
+            
+        }
+    }
+    
+}

4ffd273d6a2770bff821e437b99c8f682381f707
yusran <jhyyusran766@gmail.com>
2024-10-25 21:10:14 +0800

    Day54

---
+++ Days54.java
@@ -0,0 +1,19 @@
+
+package pkg100dayscoding;
+
+import java.util.Scanner;
+
+public class Days54 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("masukan nilai batas akhir: ");
+        int a = input.nextInt();
+        for (int i = 0; i <= a; i++) {
+            System.out.print("*");
+            
+        }
+        
+        
+    }
+ 
+}

53f4b254891ff9ce6ab82a98c1c943277604d2c5
yusran <jhyyusran766@gmail.com>
2024-10-30 22:05:22 +0800

    Day59

---
+++ Days59.java
@@ -0,0 +1,10 @@
+
+package pkg100dayscoding;
+public class Days59 {
+    public static void main(String[] args) {
+    int [] a = {1, 2, 3, 4, 5};
+    for (int b : a){
+    System.out.println(b);
+      }
+    }
+}

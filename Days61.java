7feced5c5c26d1c5b3fcad3324710377cd6f52dc
yusran <jhyyusran766@gmail.com>
2024-11-01 20:55:28 +0800

    Add files via upload

---
+++ Days61.java
@@ -0,0 +1,11 @@
+
+package pkg100dayscoding;
+public class Days61 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            i -= i;
+            System.out.println(i); 
+        }
+    }
+ 
+}

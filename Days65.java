f2e63e0a2dff5e317b692068954e65689593aeb2
yusran <jhyyusran766@gmail.com>
2024-11-05 21:54:45 +0800

    Day65

---
+++ Days65.java
@@ -0,0 +1,11 @@
+
+package pkg100dayscoding;
+public class Days65 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 1; j < 3; j++) {
+                System.out.print(j + " " + i );
+            }
+        }
+    }
+}

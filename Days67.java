6c70f73260e089e30bd0d087ce688481db912388
yusran <jhyyusran766@gmail.com>
2024-11-07 22:22:16 +0800

    Day67

---
+++ Days67.java
@@ -0,0 +1,13 @@
+
+package pkg100dayscoding;
+public class Days67 {
+   public static void   method() {
+    String nama = "Yusran";
+    int umur = 18;
+    System.out.print("nama saya adalah "+nama+" dan berumur "+umur);
+  }
+
+  public static void main(String[] args) {
+    method();
+  }
+}

6ba6f3235e8218e5fd8aa51baa2ed4855919e9e8
yusran <jhyyusran766@gmail.com>
2024-11-08 20:52:00 +0800

    Day68

---
+++ Days68.java
@@ -0,0 +1,15 @@
+
+package pkg100dayscoding;
+public class Days68 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int d = 30;
+        int c = aku(a, b , d);
+        System.out.println(a +"+" + b +"+"+ d + "=" + c);
+                
+    }
+    public static int aku(int x, int y, int z){ 
+        return x + y + z;     
+ }
+}

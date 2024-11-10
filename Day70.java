f52829982936b6213e980ffbdd2eb1c84287aebb
yusran <jhyyusran766@gmail.com>
2024-11-10 23:14:35 +0800

    Day70

---
+++ Day70.java
@@ -0,0 +1,9 @@
+
+package pkg100dayscoding;
+public class Day70 {
+    public static String yu(String nama, int umur){
+        return nama + " berumur "+ umur + " tahubn";
+    }public static void main(String[] args) {
+            System.out.println(yu("yusran", 18));
+    }
+}

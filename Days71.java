a4e1f0a051000fc86e21e9510ca5f4c914284f93
yusran <jhyyusran766@gmail.com>
2024-11-11 22:51:16 +0800

    Days71.java

--- Days71.java
+++ Days71.java
@@ -3,7 +3,7 @@ package pkg100dayscoding;
 public class Days71 {
     public static void main(String[] args) {
         yu();
-    }public static void yu(){
+    }static void yu(){
             System.out.println("Yusran");
     }
 }

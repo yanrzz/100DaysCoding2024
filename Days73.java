0e27c5c1c5831e4fe95552536a42bf74b42bf653
yusran <jhyyusran766@gmail.com>
2024-11-13 23:18:56 +0800

    Day73

---
+++ Days73.java
@@ -0,0 +1,14 @@
+
+package pkg100dayscoding;
+public class Days73 {
+    public static void main(String[] args) {
+        String nama = "yusran";
+        char y = nama.charAt(0);
+        char u = nama.charAt(1);
+        char s = nama.charAt(2);
+        char r = nama.charAt(3);
+        char a = nama.charAt(4);
+        char n = nama.charAt(5);
+        System.out.println("perkenalkan nama saya "+ y+u+s+r+a+n);
+    }
+}

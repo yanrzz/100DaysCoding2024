cc208e9eeca485e72e1c217ad4899a6a7d0f7144
yusran <jhyyusran766@gmail.com>
2024-11-14 17:44:21 +0800

    Day74

---
+++ Days74.java
@@ -0,0 +1,13 @@
+
+package pkg100dayscoding;
+
+import java.util.Scanner;
+
+public class Days74 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("masukan kata");
+        String a = in.nextLine();
+        System.out.println("maka panjang kata nya adalah " + a.length());
+    }
+}

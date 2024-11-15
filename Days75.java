a19e86c2ba32a4730f80f81bf48fbaf9235b7256
yusran <jhyyusran766@gmail.com>
2024-11-15 21:24:55 +0800

    Day75

---
+++ Days75.java
@@ -0,0 +1,15 @@
+
+package pkg100dayscoding;
+
+import java.util.Scanner;
+
+public class Days75 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukan kata: ");
+        String a = input.nextLine();
+        String b = a.toLowerCase();
+        System.out.println(b);
+    }
+ 
+}

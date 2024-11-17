e5c8f4839e0bd0229439f72dd3652dcb00d7b213
yusran <jhyyusran766@gmail.com>
2024-11-17 20:03:13 +0800

    Day77

---
+++ Days77.java
@@ -0,0 +1,14 @@
+
+package pkg100dayscoding;
+
+import java.util.Scanner;
+
+public class Days77 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukan kata: ");
+        String a = input.nextLine();
+        String b = "aku";
+        System.out.println(a.equals(b));
+    }
+}

88b9292f864b6f904281693563253214d4c3ae11
yusran <jhyyusran766@gmail.com>
2024-11-22 21:59:25 +0800

    Day82

---
+++ Days82.java
@@ -0,0 +1,13 @@
+
+package pkg100dayscoding;
+public class Days82 {
+    public static void main(String[] args) {
+        double a = 6.5;
+        double b = Math.ceil(a);
+        double c = Math.floor(a);
+        double d = Math.round(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+    }
+}

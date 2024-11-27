cbbf01a2327f90e743c8f29d4d924c9b7fa1bfbe
yusran <jhyyusran766@gmail.com>
2024-11-27 20:33:03 +0800

    Day87

---
+++ Days87.java
@@ -0,0 +1,9 @@
+
+package pkg100dayscoding;
+public class Days87 {
+    public static void main(String[] args) {
+        int [] a = {1,2,3};
+        a[0] = 4;
+        System.out.println(a[0]);
+    }
+}
